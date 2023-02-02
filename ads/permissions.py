03b9d73c1ba3f639553b87b08834888fc50bae0e
mvladlena85 <mvladlena@yandex.ru>
2023-02-03 02:36:41 +0300

    complete homework

--- ads/permissions.py
+++ ads/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+from ads.models import Ads
+
 
 class IsSelectionOwner(permissions.BasePermission):
     message = "Вы не можете редактировать эту подборку"
@@ -13,7 +15,14 @@ class IsSelectionOwner(permissions.BasePermission):
 class IsAdOwnerOrStaff(permissions.BasePermission):
     message = "Вы не можете редактировать это объявление"
 
+    def has_permission(self, request, view):
+        ad = Ads.objects.get(id=view.kwargs.get('pk'))
+        if request.user == ad.author or request.user.role in ['moderator', 'admin']:
+            return True
+        return False
+
     def has_object_permission(self, request, view, obj):
         if request.user == obj.author or request.user.role in ['moderator', 'admin']:
             return True
         return False
+
